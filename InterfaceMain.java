ecfdcd904b7a349d730b2ff31844017eb852ae6c
Blackcode2 <doky03115@gmail.com>
2022-04-26 21:31:41 +0900

    created and coded InterfaceMain.java

---
+++ InterfaceMain.java
@@ -0,0 +1,17 @@
+interface Calculable {
+	int sum(int x, int y);
+}
+
+class RealCal implements Calculable {
+	public int sum(int x, int y) {
+		return x + y;
+	}
+}
+
+
+public class InterfaceMain {
+	public static void main(String[] args) {
+		RealCal c = new RealCal();
+		System.out.println(c.sum(2, 1));
+	}
+}
